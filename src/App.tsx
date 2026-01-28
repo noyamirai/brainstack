91eb1a30b9c12b8b1b87067a03d21f2dea8d5cb5
Maijla Ikiz <maijla@activo.nl>
2026-01-28 17:31:50 +0100

    added: dual panel

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import NavHeader from "./components/NavHeader";
 import Aside from "./components/Aside";
 import Menu from "./components/Menu";
 import Stack from "./views/Stack";
+import Workspace from "./views/Workspace";
 
 const AppLayout: React.FC = () => {
     const { currentPath, currentView } = useCurrentView();
@@ -37,6 +38,7 @@ const AppLayout: React.FC = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/stack" element={<Stack />} />
+                <Route path="/workspace" element={<Workspace />} />
             </Routes>
         </div>
     );
